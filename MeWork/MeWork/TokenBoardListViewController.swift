09317e493e44335dea67c59ce63e81901c3b9166
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-06-24 15:45:17 -0400

    Add TokenBoardListVC class

---
+++ MeWork/MeWork/TokenBoardListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TokenBoardListViewController.swift
+//  MeWork
+//
+//  Created by Joanna Lingenfelter on 6/24/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+import UIKit
+
+class TokenBoardListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
