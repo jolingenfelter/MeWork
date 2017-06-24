6891417204c9e7a28793a1a5f9bfcf16368494a1
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-06-24 19:50:45 -0400

    TokenBoardFetchedResultsController class implementation

--- MeWork/MeWork/TokenBoardFetchedResultsController.swift
+++ MeWork/MeWork/TokenBoardFetchedResultsController.swift
@@ -9,6 +9,41 @@
 import UIKit
 import CoreData
 
-class TokenBoardFetchedResultsController: NSFetchedResultsController<NSFetchRequestResult> {
+class TokenBoardFetchedResultsController: NSFetchedResultsController<NSFetchRequestResult>, NSFetchedResultsControllerDelegate {
+    
+    private let tableView: UITableView
+    
+    init(fetchRequest: NSFetchRequest<NSFetchRequestResult>, managedObjectContext: NSManagedObjectContext, tableView: UITableView) {
+        
+        self.tableView = tableView
+        
+        super.init(fetchRequest: fetchRequest, managedObjectContext: managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        
+        self.delegate = self
+        
+        executeFetch()
+        
+    }
+    
+    func executeFetch() {
+        
+        do {
+            
+            try performFetch()
+            
+        } catch let error as NSError {
+            
+            print("Unresolved error: \(error.localizedDescription)")
+            
+        }
+        
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.reloadData()
+    }
 
 }
+
