09480d9225d086d6ea22a0b60ae281ae338e51bf
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-06-30 11:36:32 -0400

    Begin backgroundColorVC setup

--- MeWork/MeWork/BackgroundColorViewController.swift
+++ MeWork/MeWork/BackgroundColorViewController.swift
@@ -9,6 +9,49 @@
 import UIKit
 
 class BackgroundColorViewController: UIViewController {
+    
+    var childName: String?
+    var tokenNumber: Int?
+    var selectedColor: Color?
+    var tokenBoard: TokenBoard?
+    
+    lazy var titleLabel: UILabel = {
+        
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(label)
+        
+        return label
+        
+    }()
+    
+    lazy var colorPaletteView: UICollectionView = {
+        
+        let collectionView = UICollectionView()
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        collectionView.isScrollEnabled = false
+        
+        self.view.addSubview(collectionView)
+        
+        return collectionView
+        
+    }()
+    
+    convenience init(childName: String, numberOfTokens: Int) {
+        self.init()
+        self.childName = childName
+        self.tokenNumber = numberOfTokens
+    }
+    
+    convenience init(tokenBoard: TokenBoard) {
+        self.init()
+        self.tokenBoard = tokenBoard
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
