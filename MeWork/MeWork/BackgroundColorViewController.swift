370e8c985ca9245853355fec7228b1ef73babf71
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-07-04 14:44:41 -0400

    Constraints for titleLabel & collectionView of BackgroundColorVC

--- MeWork/MeWork/BackgroundColorViewController.swift
+++ MeWork/MeWork/BackgroundColorViewController.swift
@@ -31,7 +31,8 @@ class BackgroundColorViewController: UIViewController {
     
     lazy var colorPaletteView: UICollectionView = {
         
-        let collectionView = UICollectionView()
+        let flowLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: flowLayout)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = .white
         collectionView.isScrollEnabled = false
@@ -55,9 +56,27 @@ class BackgroundColorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = Color.green.color()
+        
+        prepareViewController()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func prepareViewController() {
+        
+        if  let tokenBoard = tokenBoard {
+            
+            titleLabel.text = "Edit background color"
+            selectedColor = Color(rawValue: tokenBoard.backgroundColor!)!
+            view.backgroundColor = Color(rawValue: tokenBoard.backgroundColor!)!.color()
+            
+        } else {
+            
+            titleLabel.text = "Choose a background color"
+            view.backgroundColor = Color.green.color()
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,11 +85,67 @@ class BackgroundColorViewController: UIViewController {
     }
     
     override func viewDidLayoutSubviews() {
+        
         super.viewDidLayoutSubviews()
+        
+        switch UIDevice.current.userInterfaceIdiom {
+            
+        case .pad:
+            
+            if DeviceType.IS_IPAD_PRO_12_9 {
+                
+                setupLabel(withFontSize: 60)
+                setupCollectionView(withTopConstant: 80, andBottomConstant: 200)
+                
+            } else {
+                
+                setupLabel(withFontSize: 48)
+                setupCollectionView(withTopConstant: 80, andBottomConstant: 120)
+                
+            }
+            
+        case .phone:
+            
+            setupLabel(withFontSize: 24)
+            
+            if DeviceType.IS_IPHONE_6P || DeviceType.IS_IPHONE_7P {
+                
+                setupCollectionView(withTopConstant: 60, andBottomConstant: 100)
+                
+            } else {
+                
+                setupCollectionView(withTopConstant: 40, andBottomConstant: 40)
+                
+            }
+            
+        default:
+            
+            break
+            
+        }
     }
     
     func setupLabel(withFontSize: CGFloat) {
         
+        titleLabel.font = UIFont.systemFont(ofSize: withFontSize)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 40)
+            ])
+        
+    }
+    
+    func setupCollectionView(withTopConstant: CGFloat, andBottomConstant: CGFloat) {
+        
+        NSLayoutConstraint.activate([
+            colorPaletteView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            colorPaletteView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            colorPaletteView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: withTopConstant),
+            colorPaletteView.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: andBottomConstant)
+            ])
+
     }
     
 }
