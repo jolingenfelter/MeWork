e1b953400d213a99bf209a8a5c962eec6ff0773a
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-11-21 20:52:06 -0500

    Laying out ChooseRewardVC

--- MeWork/MeWork/ChooseRewardViewController.swift
+++ MeWork/MeWork/ChooseRewardViewController.swift
@@ -15,6 +15,63 @@ protocol ChooseRewardDelegate {
 class ChooseRewardViewController: UIViewController {
     
     var delegate: ChooseRewardDelegate!
+    
+    lazy var rewardImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 10
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 10
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(imageView)
+        
+        return imageView
+        
+    }()
+    
+    lazy var imageFromWebButton: UIButton = {
+        
+        let button = UIButton()
+        button.backgroundColor = Color.navBarBlue.color()
+        button.setTitle("Image from the Web", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(imageFromWeb), for: .touchUpInside)
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(button)
+        
+        return button
+        
+    }()
+    
+    lazy var imageFromPhotoLibraryButton: UIButton = {
+        
+        let button = UIButton()
+        button.backgroundColor = Color.navBarBlue.color()
+        button.setTitle("Image from Photos", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(imageFromPhotoLibrary), for: .touchUpInside)
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(button)
+        
+        return button
+        
+    }()
+    
+    lazy var getWebImageViewController:  GetWebImageViewController = {
+        let getWebImageVC = GetWebImageViewController()
+        //getWebImageVC.delegate = self
+        return getWebImageVC
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +81,90 @@ class ChooseRewardViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        switch UIDevice.current.userInterfaceIdiom {
+            
+            // Layout for iPads
+            
+        case .pad:
+            
+            // iPad Pro 12.9 inch
+            if ScreenSize.SCREEN_MAX_LENGTH == 1366.0 {
+                
+                setupImageView(withHeight: 440, andTopAnchorConstant: 60)
+                buttonSetup(withFontSize: 28, height: 80, topAnchorConstant: 180, bottomAnchorConstant: -220)
+                
+                // iPad Pro 10.5 inch
+            } else if ScreenSize.SCREEN_MAX_LENGTH == 1112.0 {
+                
+                setupImageView(withHeight: 400, andTopAnchorConstant: 60)
+                buttonSetup(withFontSize: 24, height: 60, topAnchorConstant: 80, bottomAnchorConstant: -180)
+                
+                // iPad 9.7 inch and mini
+            } else {
+                
+                setupImageView(withHeight: 400, andTopAnchorConstant: 60)
+                buttonSetup(withFontSize: 20, height: 50, topAnchorConstant: 80, bottomAnchorConstant: -120)
+                
+            }
+            
+            // Layout for iPhones
+            
+        case .phone:
+            
+            // iPhone 5 Size
+            if ScreenSize.SCREEN_MAX_LENGTH == 568.0 {
+                
+                setupImageView(withHeight: 170, andTopAnchorConstant: 20)
+                buttonSetup(withFontSize: 14, height: 40, topAnchorConstant: 40, bottomAnchorConstant: -40)
+                
+                // iPhone 6 Size
+            } else if ScreenSize.SCREEN_MAX_LENGTH == 667 {
+                
+                setupImageView(withHeight: 180, andTopAnchorConstant: 20)
+                buttonSetup(withFontSize: 16, height: 40, topAnchorConstant: 80, bottomAnchorConstant: -80)
+                
+                // iPhone X Size
+            } else if ScreenSize.SCREEN_MAX_LENGTH == 812.0 {
+                
+                setupImageView(withHeight: 260, andTopAnchorConstant: 40)
+                buttonSetup(withFontSize: 18, height: 40, topAnchorConstant: 40, bottomAnchorConstant: -120)
+                
+                // iPhone Plus Size
+            } else {
+                
+                setupImageView(withHeight: 240, andTopAnchorConstant: 40)
+                buttonSetup(withFontSize: 18, height: 40, topAnchorConstant: 80, bottomAnchorConstant: -80)
+                
+            }
+            
+        default:
+            
+            break
+            
+        }
+        
+        
+    }
+
+    
+    func setupImageView(withHeight: CGFloat, andTopAnchorConstant: CGFloat) {
+        
+        rewardImageView.backgroundColor = UIColor.blue
+        NSLayoutConstraint.activate([
+            rewardImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: andTopAnchorConstant),
+            rewardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            rewardImageView.heightAnchor.constraint(equalToConstant: withHeight),
+            rewardImageView.widthAnchor.constraint(equalToConstant: withHeight)])
+        
+    }
+    
+    func buttonSetup(withFontSize: CGFloat, height: CGFloat, topAnchorConstant: CGFloat, bottomAnchorConstant: CGFloat) {
+        
+    }
+    
     func navBarSetup() {
         
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(donePressed))
@@ -31,6 +172,14 @@ class ChooseRewardViewController: UIViewController {
         
         navigationItem.leftBarButtonItem = cancelButton
         navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    @objc func imageFromWeb() {
+        
+    }
+    
+    @objc func imageFromPhotoLibrary() {
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,10 +194,10 @@ class ChooseRewardViewController: UIViewController {
 extension ChooseRewardViewController {
     
     @objc func donePressed() {
-        
+        self.dismiss(animated: true, completion: nil)
     }
     
     @objc func cancelPressed() {
-        
+        self.dismiss(animated: true, completion: nil)
     }
 }
